ef7263dcfd7f7c4695836d58c6c26502d0a7c99d
ValentinAlexandrovGeorgiev <valentin.georgiev95@abv.bg>
2015-10-22 15:57:52 +0300

    Implemented Cards

--- Santase/Santase.ConsoleUI/Program.cs
+++ Santase/Santase.ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Santase.Logic.Cards;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,14 @@ using System.Threading.Tasks;
 
 namespace Santase.ConsoleUI
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            var card = new Card(CardSuit.Heart, CardType.Ace);
+            var card2 = new Card(CardSuit.Heart, CardType.Ace);
+            Console.WriteLine(card.ToString());
+            Console.WriteLine(card.Equals(card2));
         }
     }
 }
